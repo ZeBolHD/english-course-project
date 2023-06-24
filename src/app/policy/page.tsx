4bba9343dd921c5004710288e241f7418633e488
ZeBolHD <zezebolz@gmail.com>
2023-06-24 09:50:22 +0300

    added Policy-page

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,11 @@
+const PolicyPage = () => {
+  return (
+    <div className="mt-[100px]">
+      <h2 className="font-black text-[80px] text-primary-2 uppercase tracking-wider drop-shadow-text">
+        Политика конфидациальности
+      </h2>
+    </div>
+  );
+};
+
+export default PolicyPage;
