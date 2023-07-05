d582dcba2eb14b1a35d76f48a3cd5aaa1917ccb2
ZeBolHD <zezebolz@gmail.com>
2023-07-05 18:22:18 +0300

    added api key to revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,16 @@
 import { revalidate } from "@/helpers/revalidate";
 import { NextRequest, NextResponse } from "next/server";
 
+const REVALIDATION_KEY = process.env.REVALIDATION_KEY as string;
+
 export async function POST(request: NextRequest) {
   const req = await request.json();
+
+  const revalidationKey = request.nextUrl.searchParams.get("key");
+
+  if (revalidationKey !== REVALIDATION_KEY) {
+    return NextResponse.json({ error: "invalid api key" }, { status: 403 });
+  }
 
   const model = req.model;
   const slug = req.entry?.slug;
